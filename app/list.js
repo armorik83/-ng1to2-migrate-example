545a6d0d3b6d90ef6d73dff69487f3c1f63ce4b0
OKUNOKENTARO <okunokentaro@me.com>
2016-07-18 14:57:52 +0900

    base of angular 1

---
+++ app/list.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+  angular.module('population').directive('ppList', function() {
+    return {
+      restrict: 'E',
+      template: '<p>list</p>',
+      controllerAs: '$ctrl',
+      controller: function listController() {
+        //
+      }
+    };
+  });
+})();
