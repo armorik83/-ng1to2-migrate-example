ef757fc6098bccb442e431771f8b8fa9d3c78be8
OKUNOKENTARO <okunokentaro@me.com>
2016-07-18 17:20:57 +0900

    implement onClick

--- app/country-row.js
+++ app/country-row.js
@@ -7,14 +7,26 @@
       return {
         restrict: 'E',
         template:
-        '<img ng-src="{{$ctrl.flagSrc}}">{{$ctrl.code | codeToName}} {{$ctrl.population | number}}',
+        '<img ng-src="{{$ctrl.flagSrc}}">' +
+        '{{$ctrl.code | codeToName}} {{$ctrl.population | number}}',
         bindToController: {
           code: "=",
           population: "="
         },
         controllerAs: '$ctrl',
         controller: [
-          function countryRowController() {
+          '$scope',
+          '$element',
+          function countryRowController($scope, $element) {
+            function onClick() {
+              console.log('clicked!');
+            }
+
+            $element.bind('click', onClick);
+            $scope.$on('$destroy', function() {
+              $element.unbind('click', onClick);
+            });
+
             this.flagSrc = 'app/resources/' + this.code + '.png';
           }
         ]
